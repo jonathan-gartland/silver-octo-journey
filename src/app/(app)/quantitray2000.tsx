1ee18bc3f843a54f9f53317cef35c9a78120b70a
jonathan-gartland <jonathan-gartland@gmail.com>
2024-10-24 18:52:55 -0400

    chore: save state, qt2k view rework

--- src/app/(app)/quantitray2000.tsx
+++ src/app/(app)/quantitray2000.tsx
@@ -1,38 +1,50 @@
 import type { Mpn } from 'mpn-lookup/mpn.lookup';
 import { getQt2KMpn } from 'mpn-lookup/mpn.lookup';
-import { useState } from 'react';
-import { StyleSheet, TextInput } from 'react-native';
+import React, { type FC, useState } from 'react';
+import { StyleSheet, TextInput, type ViewProps } from 'react-native';
 
 import { Text, View } from '@/ui';
+
+function qt2KMpn(inLarge: number, inSmall: number) {
+  let mpn2KList: Mpn = [];
+  if (inLarge >= 0 && inLarge <= 49 && inSmall >= 0 && inSmall <= 48) {
+    mpn2KList = getQt2KMpn(inLarge, inSmall);
+  }
+  return (
+    <View style={{}}>
+      <Text>MPN:</Text>
+      {mpn2KList && mpn2KList.length === 0 && (
+        <Text data-testid={'empty-value-text'}>Enter valid values please!</Text>
+      )}
+      <Text>{mpn2KList !== undefined && mpn2KList[0]}</Text>
+      <Text>95% Confidence Interval:</Text>
+      <Text>High: {mpn2KList !== undefined && mpn2KList[2]}</Text>
+      <Text>Low: {mpn2KList !== undefined && mpn2KList[1]}</Text>
+    </View>
+  );
+}
+
+const TitleView: FC<ViewProps> = () => (
+  <Text style={{ ...stylesQt.title, paddingTop: 10 }}>
+    QuantiTray2000&reg; MPN
+  </Text>
+);
+
+const MpnView2K: FC<{ large: number; small: number }> = ({ large, small }) => (
+  <View>{qt2KMpn(large, small)}</View>
+);
+
 export default function Qt2000Mpn() {
   const [in2KL, setIn2KL] = useState(0);
   const [in2KS, setIn2KS] = useState(0);
-
-  function qt2KMpn(inLarge: number, inSmall: number) {
-    let mpn2KList: Mpn = [];
-    if (inLarge >= 0 && inLarge <= 49 && inSmall >= 0 && inSmall <= 48) {
-      mpn2KList = getQt2KMpn(inLarge, inSmall);
-    }
-    return (
-      <View style={{}}>
-        <Text>MPN:</Text>
-        {mpn2KList && mpn2KList.length === 0 && (
-          <Text data-testid={'empty-value-text'}>
-            Enter valid values please!
-          </Text>
-        )}
-        <Text>{mpn2KList !== undefined && mpn2KList[0]}</Text>
-        <Text>95% Confidence Interval:</Text>
-        <Text>High: {mpn2KList !== undefined && mpn2KList[2]}</Text>
-        <Text>Low: {mpn2KList !== undefined && mpn2KList[1]}</Text>
-      </View>
-    );
-  }
-  return (
+  const handleLargeNumberChange = (value: string) => {
+    setIn2KL(Number(value));
+  };
+  const handleSmallNumberChange = (value: string) => {
+    setIn2KS(Number(value));
+  };
+  const InputView: FC<ViewProps> = () => (
     <View>
-      <Text style={{ ...stylesQt.title, paddingTop: 10 }}>
-        QuantiTray2000&reg; MPN
-      </Text>
       <View
         style={{
           paddingLeft: 10,
@@ -49,9 +61,7 @@ export default function Qt2000Mpn() {
               borderWidth: 1,
               textAlign: 'center',
             }}
-            onChangeText={(text) => {
-              setIn2KL(Number(text));
-            }}
+            onChangeText={handleLargeNumberChange}
             value={in2KL.toString()}
             keyboardType="numeric"
           />
@@ -64,13 +74,20 @@ export default function Qt2000Mpn() {
               borderWidth: 1,
               textAlign: 'center',
             }}
-            onChangeText={(text) => setIn2KS(Number(text))}
+            onChangeText={handleSmallNumberChange}
             value={in2KS.toString()}
             keyboardType="numeric"
           />
         </View>
-        {qt2KMpn(in2KL, in2KS)}
       </View>
+    </View>
+  );
+
+  return (
+    <View>
+      <TitleView />
+      <InputView />
+      <MpnView2K large={in2KL} small={in2KS} />
     </View>
   );
 }
